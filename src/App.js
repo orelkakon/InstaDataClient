1579eb4007b589db16a25f4cb8e70fc01cc4b767
orelkakon <orelkak@post.bgu.ac.il>
2021-08-01 01:37:11 +0300

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import Header from './Components/Header.js'
 import Footer from './Components/Footer.js'
 import Body from './Components/Body.js'
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App () {
+
+function App() {
   return (
     <div className="App">
-        <Header/>
-        <Body/>
-        <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact strict render={
+          () => (
+            <Body/>
+          )
+        } />
+        <Route path="/Info" exact strict render={
+          () => (
+            <Body/>
+          )
+        } />
+        <Route path="/About" exact strict render={
+          () => (
+            <Body/>
+          )
+        } />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 const Header = () => {
     return (
-        <div>
+        <div className="navMenu">
+            <NavLink className="a" to="/" style={{ textDecoration: 'none', color: 'white', textShadow: '2px 2px gray' }} activeClassName="nl" exact activeStyle={
+                { textDecoration: 'underline' }
+            }>Sign In</NavLink>
+            
+            <NavLink className="a" to="/Info" style={{ textDecoration: 'none', color: 'white', textShadow: '2px 2px gray' }} activeClassName="nl" exact activeStyle={
+                { textDecoration: 'underline' }
+            }>Information</NavLink>
 
+            <NavLink className="a" to="/About" style={{ textDecoration: 'none', color: 'white', textShadow: '2px 2px gray' }} activeClassName="nl" exact activeStyle={
+                { textDecoration: 'underline' }
+            }>About And Contact</NavLink>
+            <div class="dot"></div>
         </div>
     )
 }
