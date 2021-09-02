ea607f5444132b634cafc9dcc0a3c9014259398a
orelkakon <orelkak@post.bgu.ac.il>
2021-09-02 15:35:10 +0300

    add github

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,6 +3,7 @@ import phoneLogo from './../Assets/phone_logo.png'
 import mailLogo from './../Assets/mail.jpg'
 import instaLogo from './../Assets/insta.png'
 import linkedinLogo from './../Assets/linkedin.png'
+import githubLogo from './../Assets/github.png'
 
 const About = () => {
     return (
@@ -19,10 +20,11 @@ const About = () => {
                 </p>
                 <br /><br />
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <a href="https://www.instagram.com/orelkakon/" style={{ textDecoration: "none", fontSize: "33px", color: "#fff", paddingRight: '2px'  }}> <img src={instaLogo} alt="instaLogo" height="33px" /></a>
+                    <a href="https://www.instagram.com/orelkakon/" target="_blank" rel="noreferrer" style={{ textDecoration: "none", fontSize: "33px", color: "#fff", paddingRight: '2px'  }}> <img src={instaLogo} alt="instaLogo" height="33px" /></a>
                     <a href="mailto:orelkak@post.bgu.ac.il" style={{ textDecoration: "none", fontSize: "33px", color: "#fff", paddingRight: '6px' }}> <img src={mailLogo} alt="phoneLogo" height="30px" /></a>
                     <a href="tel:+97254-4650-494" style={{ textDecoration: "none", fontSize: "33px", color: "#fff", paddingRight: '2px' }}> <img src={phoneLogo} alt="phoneLogo" height="30px" /></a>
-                    <a href="https://il.linkedin.com/in/orel-kakon-323928218" style={{ textDecoration: "none", fontSize: "33px", color: "#fff" }}> <img src={linkedinLogo} alt="linkedinLogo" height="36px" /></a>
+                    <a href="https://il.linkedin.com/in/orel-kakon-323928218" target="_blank" rel="noreferrer" style={{ textDecoration: "none", fontSize: "33px", color: "#fff", paddingRight: '1px' }}> <img src={linkedinLogo} alt="linkedinLogo" height="36px" /></a>
+                    <a href="https://github.com/orelkakon" target="_blank" rel="noreferrer" style={{ textDecoration: "none", fontSize: "28px", color: "#fff" }}> <img src={githubLogo} alt="githubLogo" height="36px" /></a>
                 </div>
                 <br/><br/><br/><br/><br/>
             </div>
